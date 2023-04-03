fa5ddaf3c3583b669d674f212f91d6df1e74bdd5
Alejandro Barros <alobarros8@gmail.com>
2023-04-03 13:01:25 -0300

    cambios item

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
+import "./ItemlistContainer";
+const Item = (props) => {
+  return (
+    <>
+      <article className="list-item">
+        <h4 className="card-title">{props.nombre}</h4>
+        <img src="" alt="" srcset="" />
 
-let Item = (props) => {
-  <>
-    <article className="list-item">
-      <h4 className="card-title">{props.nombre}</h4>
-      <img src="" alt="" srcset="" />
-
-      <p className="card-description">{props.descripcion}</p>
-      <p className="precio">${props.precio}</p>
-    </article>
-    ;
-  </>;
+        <p className="card-description">{props.descripcion}</p>
+        <p className="precio">${props.precio}</p>
+      </article>
+    </>
+  );
 };
 export default Item;
