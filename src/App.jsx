a4c7db07f822da0c04f7556950b6d7163b53f2b2
Alejandro Barros <alobarros8@gmail.com>
2023-04-03 12:06:29 -0300

    CAMBIO PROPS

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,13 @@ import "./App.css";
 import Title from "./Title";
 import Navbarjs from "./Header";
 import ItemlistContainer from "./ItemlistContainer";
-import Item from "./Item";
+
 function App() {
   return (
     <div>
       <Navbarjs />
       <Title />
-      <ItemlistContainer>
-        <Item
-          nombre="CheeseBurger"
-          imagen=""
-          descripcion="Doble carne, Queso cheddar"
-          precio="1600"
-        />
-      </ItemlistContainer>
+      <ItemlistContainer />
     </div>
   );
 }
--- src/ItemlistContainer.jsx
+++ src/ItemlistContainer.jsx
@@ -1,6 +1,18 @@
 import "./ItemlistContainer.css";
-
+import Item from "./Item";
 function ItemlistContainer() {
-  return <div className="item-list-container"></div>;
+  let nombreburger = "cheeseburger";
+  let descripcionburger = "Doble carne, Queso cheddar";
+  let precioburger = "1600";
+  return (
+    <div className="item-list-container">
+      <Item
+        nombre={nombreburger}
+        imagen=""
+        descripcion={descripcionburger}
+        precio={precioburger}
+      />
+    </div>
+  );
 }
 export default ItemlistContainer;
